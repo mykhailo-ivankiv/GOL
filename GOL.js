cd9d453222362fc1d305f6fc4bde5b7d99f7b86c
Mykhailo Ivankiv <neformal.lviv@gmail.com>
2017-11-25 19:44:36 +0200

    Step 1

---
+++ GOL.js
@@ -0,0 +1,13 @@
+const  render = (board) => "";
+const nextStep = (board) => {
+    const newBoard = []
+    return newBoard;
+}
+
+const getNeibours = (board, [x, y]) => {
+    return 1;
+}
+
+const getNextCellState = (board, [x, y]) => {
+    return 0 || 1;
+}
